1063a02821fab5d8c5aeace8366ebdeb9033a460
Christian Bogro <cb@link2b.fr>
2017-10-27 22:56:22 +0000

    commit 27/10/17 n:4

--- src/Link2b/CoreBundle/Controller/PlatformController.php
+++ src/Link2b/CoreBundle/Controller/PlatformController.php
@@ -4,6 +4,7 @@ namespace Link2b\CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Link2b\CoreBundle\Entity\Platform;
 
 
 class PlatformController extends Controller
@@ -28,7 +29,15 @@ class PlatformController extends Controller
 
     public function finishDeploiementAction(Request $request)
     {
-        return $this->render('CoreBundle:Platform:finishDeploiement.html.twig');
+        //$platform
+        $id = $request->query->get('id');
+
+        $platform = $this->getDoctrine()->getRepository(Platform::class)->find($id);
+
+        //echo '<pre>' . print_r($platform, true) . '</pre>';
+        //die();
+
+        return $this->render('CoreBundle:Platform:finishDeploiement.html.twig', ['platform', $platform]);
     }
 
     public function updateDeploiementAction(Request $request)
